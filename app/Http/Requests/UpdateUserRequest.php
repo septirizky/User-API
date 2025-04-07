0cb5c9295bf707ea6cb3cfc5050023ee33665c17
septi <septirizky79@gmail.com>
2025-04-07 15:11:10 +0700

    add request

---
+++ app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateUserRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name'  => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email,' . $this->route('id'),
+            'age'   => 'required|integer|min:0',
+        ];
+    }
+}
